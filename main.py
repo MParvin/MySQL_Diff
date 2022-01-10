629cd1721188a421b54a28b42ef644363cc97ad4
Mohammad Parvin <m.parvin@fidibo.com>
2022-01-10 11:04:05 +0330

    first structure added

---
+++ main.py
@@ -0,0 +1,65 @@
+from dotenv import load_dotenv
+from os.path import join, dirname
+from os import getenv
+import mysql.connector
+# Import dependencies for logging to file
+import logging
+import logging.handlers
+
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# Get variables from env file
+database_master = getenv('database_master')
+database_slave = getenv('database_slave')
+database_name = getenv('database_name')
+database_user = getenv('database_user')
+database_password = getenv('database_pass')
+log_file = getenv('log_file')
+
+# Connect to first database
+cnx = mysql.connector.connect(user=database_user, password=database_password, host=database_master, database=database_name)
+cursor = cnx.cursor()
+# Get all tables from first database
+cursor.execute("SHOW TABLES")
+tables = cursor.fetchall()
+
+# Connect to second database
+cnx2 = mysql.connector.connect(user=database_user, password=database_password, host=database_slave, database=database_name)
+cursor2 = cnx2.cursor()
+# Get all tables from second database
+cursor2.execute("SHOW TABLES")
+tables2 = cursor2.fetchall()
+
+if tables == tables2:
+    print("Databases are the same")
+else:
+    print("Databases are not the same")
+    # Log to file
+    logging.basicConfig(filename=log_file, level=logging.DEBUG)
+    logging.info("Databases are not the same")
+    logging.info("First database: " + str(tables))
+    logging.info("Second database: " + str(tables2))
+
+
+# Loop in tables
+for table in tables:
+    # Count rows in table in first database
+    cursor.execute("SELECT COUNT(*) FROM " + table[0])
+    rows_count = cursor.fetchone()[0]
+    # Count rows in table in second database
+    cursor2.execute("SELECT COUNT(*) FROM " + table[0])
+    rows_count2 = cursor2.fetchone()[0]
+    if rows_count != rows_count2:
+        print("Table " + table[0] + " is not the same")
+        # Log to file
+        logging.basicConfig(filename=log_file, level=logging.DEBUG)
+        logging.info("Table " + table[0] + " is not the same")
+        logging.info("First database: " + str(rows_count))
+        logging.info("Second database: " + str(rows_count2))
+
+cnx.close()
+cnx2.close()
+
+
