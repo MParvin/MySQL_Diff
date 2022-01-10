e61310bed23700595ac60e8813d6981fb4359a14
Mohammad Parvin <m.parvin@fidibo.com>
2022-01-10 14:03:01 +0330

    add sleep time, wait for sync

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import mysql.connector
 # Import dependencies for logging to file
 import logging
 import logging.handlers
+from time import sleep
 
 
 dotenv_path = join(dirname(__file__), '.env')
@@ -17,6 +18,7 @@ database_name = getenv('database_name')
 database_user = getenv('database_user')
 database_password = getenv('database_pass')
 log_file = getenv('log_file')
+max_time_to_sync = int(getenv('max_time_to_sync'))
 
 # Connect to first database
 cnx = mysql.connector.connect(user=database_user, password=database_password, host=database_master, database=database_name)
@@ -48,10 +50,14 @@ for table in tables:
     # Count rows in table in first database
     cursor.execute("SELECT COUNT(*) FROM " + table[0])
     rows_count = cursor.fetchone()[0]
+    # Need to sleep, because syncing is not fast in some cases
+    print("sleeping for " + str(max_time_to_sync) + " seconds")
+    sleep(max_time_to_sync)
     # Count rows in table in second database
     cursor2.execute("SELECT COUNT(*) FROM " + table[0])
     rows_count2 = cursor2.fetchone()[0]
-    if rows_count != rows_count2:
+    # if master rows is greater than slave rows, then slave is behind master
+    if rows_count > rows_count2:
         print("Table " + table[0] + " is not the same")
         # Log to file
         logging.basicConfig(filename=log_file, level=logging.DEBUG)
